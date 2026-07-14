eb44b63006a5a6c22da5e6147e1eb0641673de24
thatonevikash <vikashloharjsr@gmail.com>
2026-07-14 23:21:30 +0530

    fix: basePath

--- docs/next.config.mjs
+++ docs/next.config.mjs
@@ -5,6 +5,7 @@ const withMDX = createMDX();
 /** @type {import('next').NextConfig} */
 const config = {
   output: "export",
+  basePath: process.env.NODE_ENV === "production" ? "/create-next-mui" : "",
   reactStrictMode: true,
 };
 
